4db54e2bba65baf30c4ee3b3df981fa0e644b92c
xank <xank@qq.com>
2018-05-03 16:15:10 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App'
+
+import '@/styles/old/common.css'
+import '@/styles/cnnbuc/index.styl'
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
